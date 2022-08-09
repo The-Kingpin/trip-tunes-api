b9b01c3621343a7aa5bc0abda250a6516f2ae128
Svetoslav Ivanov <svetoslav.ivanov@windowslive.com>
2022-08-09 16:55:57 +0300

    Edit swagger configuration

--- src/main/java/com/codeharbor/triptunes/configuration/SwaggerConfiguration.java
+++ src/main/java/com/codeharbor/triptunes/configuration/SwaggerConfiguration.java
@@ -1,18 +1,13 @@
 package com.codeharbor.triptunes.configuration;
 
-
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
-@EnableAutoConfiguration
 public class SwaggerConfiguration {
     @Bean
     public Docket apiDocket() {
